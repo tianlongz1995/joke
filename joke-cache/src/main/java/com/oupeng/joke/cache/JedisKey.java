59cdcec95295747950a89e22984d7df59afe1a9e
shuangh <shuangh@oupeng.com>
2016-07-01 17:18:41 +0800

    增加段子功能

--- joke-cache/src/main/java/com/oupeng/joke/cache/JedisKey.java
+++ joke-cache/src/main/java/com/oupeng/joke/cache/JedisKey.java
@@ -5,4 +5,8 @@ public class JedisKey {
     public static final String JOKE_LIST_LIKE = "list.joke.like";
     /** 踩队列    */
     public static final String JOKE_LIST_STEP = "list.joke.step";
+    /** 反馈队列    */
+    public static final String JOKE_LIST_FEEDBACK = "list.joke.feedback";
+    /** 渠道配置信息Hash表key    */
+    public static final String JOKE_HASH_DISTRIBUTOR_CONFIG = "hash.joke.distributor.config";
 }
