ac8d2945e569e20eb87bc54a603ce9fa0cb3e735
shuangh <shuangh@oupeng.com>
2016-07-08 10:47:43 +0800

    1、增加渠道管理功能 2、增加广告管理功能 3、增加内容源管理功能

--- joke-back/src/main/java/com/oupeng/joke/back/controller/AdController.java
+++ joke-back/src/main/java/com/oupeng/joke/back/controller/AdController.java
@@ -1,6 +1,7 @@
 package com.oupeng.joke.back.controller;
 
 import com.oupeng.joke.back.service.AdService;
+import com.oupeng.joke.back.service.DistributorService;
 import com.oupeng.joke.domain.Ad;
 import com.oupeng.joke.domain.response.Result;
 import com.oupeng.joke.domain.response.Success;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -24,18 +26,66 @@ public class AdController {
 	private static final Logger logger = LoggerFactory.getLogger(AdController.class);
 	@Autowired
 	private AdService adService;
+	@Autowired
+	private DistributorService distributorService;
 
 	/**
 	 * 广告列表
 	 * @param distributorId 渠道编号
+	 * @param pos		广告位置
+	 * @param status	广告状态
+	 * @param slotId	广告位ID
 	 * @param model
 	 * @return
 	 */
 	@RequestMapping(value="/list")
-	public String getAdList(@RequestParam(value="distributorId",required=false)Integer distributorId,Model model){
+	public String getAdList(@RequestParam(value="distributorId",required=false)Integer distributorId,
+							@RequestParam(value="pos",required=false)Integer pos,
+							@RequestParam(value="status",required=false)Integer status,
+							@RequestParam(value="slotId",required=false)Integer slotId,
+							@RequestParam(value="pageNumber",required=false)Integer pageNumber,
+							@RequestParam(value="pageSize",required=false)Integer pageSize,
+							Model model){
 		try {
-			List<Ad> list = adService.getAdList(distributorId);
+			pageNumber = pageNumber == null ? 1 : pageNumber;//当前页数
+			pageSize = pageSize == null ? 10 : pageSize;//每页显示条数
+			Ad ad = new Ad();
+			ad.setStatus(status);
+			ad.setDid(distributorId);
+			ad.setPos(pos);
+			ad.setSlotId(slotId);
+			List<Ad> list = null;
+			int pageCount = 0;//总页数
+			int offset = 0 ;//开始条数index
+			int count = adService.getAdListCount(ad);//总条数
+			if(count > 0){
+				if (count % pageSize == 0) {
+					pageCount = count / pageSize;
+				} else {
+					pageCount = count / pageSize + 1;
+				}
+
+				if (pageNumber > pageCount) {
+					pageNumber = pageCount;
+				}
+				if (pageNumber < 1) {
+					pageNumber = 1;
+				}
+				offset = (pageNumber - 1) * pageSize;
+				ad.setOffset(offset);
+				ad.setPageSize(pageSize);
+				list = adService.getAdList(ad);
+			}
+
+			model.addAttribute("count", count);
+			model.addAttribute("pageNumber", pageNumber);
+			model.addAttribute("pageSize", pageSize);
+			model.addAttribute("pageCount", pageCount);
 			model.addAttribute("list", list);
+			model.addAttribute("dList", distributorService.getDistributorList(1));
+			model.addAttribute("status", status);
+			model.addAttribute("pos", pos);
+			model.addAttribute("slotId", slotId);
 			model.addAttribute("distributorId", distributorId);
 		}catch (Exception e){
 			logger.error(e.getMessage(), e);
@@ -58,44 +108,57 @@ public class AdController {
 	}
 
 	/**
-	 * 修改广告
+	 * 获取广告信息并进入修改页面
 	 * @param id
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping(value="/edit")
-	public String edit(@RequestParam(value="id")Integer id,Model model){
-		model.addAttribute("ad", adService.getAdById(id));
+	@RequestMapping(value="/modify")
+	public String modify(@RequestParam(value="id",required=true)Integer id,Model model){
+		try{
+			model.addAttribute("ad", adService.getAdById(id));
+			model.addAttribute("dList", distributorService.getDistributorList(1));
+		}catch (Exception e){
+			logger.error(e.getMessage(), e);
+		}
 		return "/ad/edit";
 	}
 
 	/**
 	 * 更新广告信息
 	 * @param id
-	 * @param name
+	 * @param slotId
+	 * @param pos
+	 * @param slide
+	 * @param did
 	 * @param status
 	 * @return
 	 */
 	@RequestMapping(value="/update")
 	@ResponseBody
 	public Result update(@RequestParam(value="id",required=true)Integer id,
-			@RequestParam(value="name",required=false)String name,
-			@RequestParam(value="status",required=true)Integer status){
-		adService.updateAd(id, name, status);
+						 @RequestParam(value="slotId",required=false)Integer slotId,
+						 @RequestParam(value="pos",required=false)Integer pos,
+						 @RequestParam(value="slide",required=true)Integer slide,
+						 @RequestParam(value="did",required=false)Integer did,
+						 @RequestParam(value="status",required=true)Integer status){
+		adService.updateAd(id, slotId, pos, slide, did,status);
 		return new Success();
 	}
 
 	/**
 	 * 新增广告
-	 * @param name
-	 * @param status
+	 * @param ad
 	 * @return
 	 */
 	@RequestMapping(value="/add")
 	@ResponseBody
-	public Result searchJokeList(@RequestParam(value="name",required=false)String name,
-			@RequestParam(value="status",required=true)Integer status){
-		adService.insertAd(name, status);
-		return new Success();
+	public Result add(Ad ad){
+		try {
+			adService.insertAd(ad);
+		}catch (Exception e){
+			logger.error(e.getMessage(), e);
+		}
+			return new Success();
 	} 
 }
