e7e9fd9f5ae2ef84aa28a451b29bd4eb09cb4273
fuhaiq <fuhaiq@oupeng.com>
2016-07-18 18:08:27 +0800

    修改cookie

--- joke-front/src/main/java/com/oupeng/joke/front/util/CookieUtil.java
+++ joke-front/src/main/java/com/oupeng/joke/front/util/CookieUtil.java
@@ -3,6 +3,8 @@ package com.oupeng.joke.front.util;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class CookieUtil {
 
 	private static final String COOKIE_NAME = "uid";
@@ -12,7 +14,7 @@ public class CookieUtil {
 		if(cookies != null && cookies.length > 0){
 			for(Cookie cookie : cookies){
 				if(COOKIE_NAME.equalsIgnoreCase(cookie.getName())){
-					return cookie.getValue();
+					return StringUtils.rightPad(cookie.getValue(), 24, "=");
 				}
 			}
 		}
