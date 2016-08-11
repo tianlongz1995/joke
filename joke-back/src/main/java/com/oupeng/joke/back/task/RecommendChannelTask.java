a2e7252c2de4ddddb359f563cc1043bcbd532d8e
chwshuang <hs_yx@foxmail.com>
2016-08-11 21:10:51 +0800

    1、解决发布推荐频道段子按天按点赞排序

--- joke-back/src/main/java/com/oupeng/joke/back/task/RecommendChannelTask.java
+++ joke-back/src/main/java/com/oupeng/joke/back/task/RecommendChannelTask.java
@@ -1,8 +1,11 @@
 package com.oupeng.joke.back.task;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,6 +19,9 @@ import com.oupeng.joke.domain.Joke;
 
 @Component
 public class RecommendChannelTask {
+
+	private static long ONE_DAY = 1000 * 60 * 60 * 24;
+	private static String FIX = "000000";
 
 	@Autowired
 	private JokeService jokeService;
@@ -31,10 +37,18 @@ public class RecommendChannelTask {
 		List<Joke> jokeList = jokeService.getJokeListForPublishRecommend();
 		if(!CollectionUtils.isEmpty(jokeList)){
 			Map<String,Double> map = Maps.newHashMap();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 			for(Joke joke : jokeList){
-				map.put(String.valueOf(joke.getId()), Double.valueOf(joke.getGood()));
+				if(joke.getVerifyTime() == null){
+					joke.setVerifyTime(new Date(new Date().getTime() - ONE_DAY));
+				}
+				if(joke.getGood() == null){
+					joke.setGood(1);
+				}
+				Long time = Long.parseLong(sdf.format(joke.getVerifyTime()) + FIX) + joke.getGood();
+				map.put(String.valueOf(joke.getId()), Double.valueOf(time));
 			}
-			jedisCache.zadd(JedisKey.SORTEDSET_RECOMMEND_CHANNEL,map);
+			jedisCache.zadd(JedisKey.SORTEDSET_RECOMMEND_CHANNEL, map);
 			map.clear();
 		}
 	}
