88a6f6f0a1c6657c0af27bdf4c38747a71d5bae5
chwshuang <hs_yx@foxmail.com>
2016-08-22 21:24:01 +0800

    1、添加下拉刷新统计功能

--- sql/joke.sql
+++ sql/joke.sql
@@ -138,3 +138,125 @@ CREATE TABLE `topic_joke` (
   `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ix_username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO users (username, password, enabled) VALUES ('admin','96e79218965eb72c92a549dd5a330112',1);
+
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `ix_auth_username` (`username`,`authority`),
+  CONSTRAINT `fk_authorities_users` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO authorities (username, authority) VALUES ('admin','ROLE_ADMIN');
+
+CREATE TABLE `channel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL COMMENT '频道名称',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态：0 有效 1 无效',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `distributor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL COMMENT '渠道名称',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态：0 有效 1 无效',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `distributor_channel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `d_id` int(11) NOT NULL COMMENT '渠道id',
+  `c_id` int(11) NOT NULL COMMENT '频道id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `distributor_label` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `d_id` int(11) NOT NULL COMMENT '渠道id',
+  `l_id` int(11) NOT NULL COMMENT '标签id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `gallery` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL COMMENT '图片标题',
+  `picture_url` varchar(255) NOT NULL COMMENT '图片链接（封面图）',
+  `picture_width` int(11) NOT NULL COMMENT '封面图片的宽度，px',
+  `picture_height` int(11) NOT NULL COMMENT '封面图片的高度，px',
+  `source_id` int(11) NOT NULL COMMENT '数据源id',
+  `picture_count` int(11) NOT NULL COMMENT '图集中图片数量',
+  `font_count` int(11) NOT NULL COMMENT '图集点赞数',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `label` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL COMMENT '标签名称',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态： 0 有效 1 无效',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `pictures` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `picture_url` varchar(255) NOT NULL COMMENT '图片链接',
+  `picture_width` int(11) NOT NULL COMMENT '图片的宽度，px',
+  `picture_height` int(11) NOT NULL COMMENT '图片的高度，px',
+  `gallery_id` int(11) NOT NULL COMMENT '图集id（标识属于同一图集）',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `source` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL COMMENT '数据源名称',
+  `url` varchar(255) NOT NULL COMMENT '数据源url',
+  `c_id` int(11) DEFAULT NULL COMMENT '频道id',
+  `l_id` int(11) DEFAULT NULL COMMENT '标签id',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态： 0 有效 1 无效',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `d_id` int(11) NOT NULL COMMENT '渠道编号',
+  `c_id` int(11) NOT NULL COMMENT '频道编号',
+  `type` int(11) NOT NULL COMMENT '反馈问题类型',
+  `content` varchar(1024) CHARACTER SET utf8 DEFAULT NULL COMMENT '反馈内容',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+-- 列表页下拉刷新日统计表 2016-08-22
+CREATE TABLE `stat_drop_total_day` (
+  `day` int(11) NOT NULL COMMENT 'int type like 20160101',
+  `pv` int(11) NOT NULL DEFAULT '0' COMMENT '列表页下拉刷新总PV',
+  `uv` int(11) NOT NULL DEFAULT '0' COMMENT '列表页下拉刷新总UV'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT '列表页下拉刷新日统计表';
+
+-- 列表页下拉刷新日统计明细表 2016-08-22
+CREATE TABLE `stat_drop_detail_day` (
+  `day` int(11) NOT NULL COMMENT 'int type like 20160101',
+  `did` varchar(30) COLLATE utf8_bin DEFAULT NULL COMMENT '渠道ID',
+  `cid` varchar(30) COLLATE utf8_bin DEFAULT NULL COMMENT '频道/标签ID',
+  `pv` int(11) NOT NULL DEFAULT '0' COMMENT '列表页下拉刷新PV',
+  `uv` int(11) NOT NULL DEFAULT '0' COMMENT '列表页下拉刷新UV'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT '列表页下拉刷新日统计明细表';
