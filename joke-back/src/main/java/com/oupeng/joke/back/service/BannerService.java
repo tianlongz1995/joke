4208b8abfae8d8e5a85e73f2676f16f90d1d514f
send <peef8ieP>
2017-02-09 10:18:08 +0800

    修改banner无法下线问题

--- joke-back/src/main/java/com/oupeng/joke/back/service/BannerService.java
+++ joke-back/src/main/java/com/oupeng/joke/back/service/BannerService.java
@@ -206,7 +206,7 @@ public class BannerService {
             int position = -1;
 
             for (int i = 0; i < list.size(); i++) {
-                if (list.get(i).getId() == id) {
+                if (list.get(i).getId().equals(id)) {
                     position = i;
                 }
             }
