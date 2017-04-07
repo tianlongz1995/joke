e6afb6cf9b13a2dd5654b0332d474af4106ea86c
zongchao <java_zong@163.com>
2017-04-07 18:11:41 +0800

    1. 改变抓取牛逼思维方式

--- spider/src/main/java/com/oupeng/joke/spider/utils/Key.java
+++ spider/src/main/java/com/oupeng/joke/spider/utils/Key.java
@@ -4,6 +4,6 @@ package com.oupeng.joke.spider.utils;
  * Created by zongchao on 2017/3/17.
  */
 public class Key {
-    public static final String BLOOM_FILTER_URL = "url过滤器";
-    public static final String BLOOM_FILTER_IMG = "图片过滤器";
+    public static final String BLOOM_FILTER_URL = "url.filter";
+    public static final String BLOOM_FILTER_IMG = "img.filter";
 }
