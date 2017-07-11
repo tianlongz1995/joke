b6ecf06c16c588d74d921b37b310fc276066f486
yeluo <1259110779@qq.com>
2017-07-11 16:06:51 +0800

    神评重爬

--- joke-back/src/main/java/com/oupeng/joke/back/task/Respider_HHmxCommentTask.java
+++ joke-back/src/main/java/com/oupeng/joke/back/task/Respider_HHmxCommentTask.java
@@ -1,12 +1,10 @@
 package com.oupeng.joke.back.task;
 
 import com.oupeng.joke.back.service.JokeService;
-import com.oupeng.joke.back.service.ManagerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -26,17 +24,22 @@ public class Respider_HHmxCommentTask {
     public void init() {
         String isRun = env.getProperty("hhmx.respider.run");
         if (isRun != null && isRun.equalsIgnoreCase("true")) {
-            RespiderTask();
+            new Thread(new RespiderThread()).start();
         }
     }
 
-    /**
-     * 2017-07-12 晚7:00开始
-     */
-    @Scheduled(cron="0 0 19 12 7 ?")
-    public void RespiderTask() {
-        logger.info("开始重新爬取joke(遨游哈哈)神评...");
-        jokeService.addJokeComment("2017-07-12 19:00:00", 148, "hhmx");
-        logger.info("重新爬取joke(遨游哈哈)神评结束");
+    class RespiderThread implements Runnable{
+        @Override
+        public void run() {
+            while(true){
+                try{
+                    logger.info("开始重新爬取joke(遨游哈哈)神评...");
+                    jokeService.addJokeComment("2017-07-12 19:00:00", 148, "hhmx");
+                    logger.info("重新爬取joke(遨游哈哈)神评结束");
+                }catch(Exception e){
+                    logger.error("重新爬取joke(内涵段子)异常:{}",e);
+                }
+            }
+        }
     }
 }
--- joke-back/src/main/java/com/oupeng/joke/back/task/Respider_NeiHanCommentTask.java
+++ joke-back/src/main/java/com/oupeng/joke/back/task/Respider_NeiHanCommentTask.java
@@ -1,13 +1,10 @@
 package com.oupeng.joke.back.task;
 
 import com.oupeng.joke.back.service.JokeService;
-import com.oupeng.joke.back.service.ManagerService;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -27,17 +24,22 @@ public class Respider_NeiHanCommentTask {
     public void init() {
         String isRun = env.getProperty("neihan.respider.run");
         if (isRun != null && isRun.equalsIgnoreCase("true")) {
-            RespiderTask();
+            new Thread(new RespiderThread()).start();
         }
     }
 
-    /**
-     * 2017-07-13 晚7:00开始
-     */
-    @Scheduled(cron="0 0 19 13 7 ?")
-    public void RespiderTask() {
-        logger.info("开始重新爬取joke(内涵段子)神评...");
-        jokeService.addJokeComment("2017-07-01 00:00:00", 155, "neihan");
-        logger.info("重新爬取joke(内涵段子)神评结束");
+    class RespiderThread implements Runnable{
+        @Override
+        public void run() {
+            while(true){
+               try{
+                   logger.info("开始重新爬取joke(内涵段子)神评...");
+                   jokeService.addJokeComment("2017-07-01 00:00:00", 155, "neihan");
+                   logger.info("重新爬取joke(内涵段子)神评结束");
+               }catch(Exception e){
+                   logger.error("重新爬取joke(内涵段子)异常:{}",e);
+               }
+            }
+        }
     }
 }
